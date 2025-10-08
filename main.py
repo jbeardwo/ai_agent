ea1f70825bbbce94c3319d60709fe798b6a41a3e
jbeardwo <jfwbeardwood@gmail.com>
2025-10-07 18:39:17 -0700

    prompting working w/ command arguments

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+import sys
+
+
 def main():
-    print("Hello from ai-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=sys.argv[1])
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}") 
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
